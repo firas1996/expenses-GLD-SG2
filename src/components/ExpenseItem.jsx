04a8aa15cdbf8f6757d6e2d626e9f0043d2d2279
firas hamila <firas.hm.1996@gmail.com>
2025-02-12 09:47:03 +0100

    a

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ExpenseItem = () => {
-  return <div></div>;
+  return (
+    <div className="expense-item">
+      <div className="expense-date">date</div>
+      <div className="expense-item__description">
+        <h2>title</h2>
+        <div className="expense-item__price">price</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
