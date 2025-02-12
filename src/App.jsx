699fcbbac551f44d2935f7c1aac0d6ab5337dbbe
firas hamila <firas.hm.1996@gmail.com>
2025-02-12 11:32:55 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const data = [
+    {
+      id: 1,
+      title: "New TV",
+      price: 799.99,
+      date: new Date("2025-02-12"),
+    },
+    {
+      id: 2,
+      title: "Voyage",
+      price: 2799.99,
+      date: new Date("2023-10-19"),
+    },
+    {
+      id: 3,
+      title: "Education",
+      price: 5000,
+      date: new Date("2024-09-20"),
+    },
+    {
+      id: 4,
+      title: "Phone",
+      price: 1500,
+      date: new Date("2025-02-7"),
+    },
+  ];
   return (
     <div>
-      <ExpenseItem />
+      {data.map((expense) => {
+        return (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            price={expense.price}
+            date={expense.date}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,19 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+const ExpenseItem = ({ price, title, date }) => {
+  const year = date.getFullYear();
+  const month = date.toLocaleString("fr-FR", { month: "long" });
+  const day = date.toLocaleString("fr-FR", { day: "2-digit" });
   return (
     <div className="expense-item">
       <div className="expense-date">
-        <div className="expense-date__month">month</div>
-        <div className="expense-date__year">year</div>
-        <div className="expense-date__day">day</div>
+        <div className="expense-date__month">{month}</div>
+        <div className="expense-date__year">{year}</div>
+        <div className="expense-date__day">{day}</div>
       </div>
       <div className="expense-item__description">
-        <h2>title</h2>
-        <div className="expense-item__price">$ price</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">$ {price}</div>
       </div>
     </div>
   );
