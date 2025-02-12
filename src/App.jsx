11557a80d824e8ded84332cd72537b485f23675b
firas hamila <firas.hm.1996@gmail.com>
2025-02-12 09:53:38 +0100

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
+      <ExpenseItem />
     </div>
   );
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
   return (
     <div className="expense-item">
-      <div className="expense-date">date</div>
+      <div className="expense-date">
+        <div className="expense-date__month">month</div>
+        <div className="expense-date__year">year</div>
+        <div className="expense-date__day">day</div>
+      </div>
       <div className="expense-item__description">
         <h2>title</h2>
-        <div className="expense-item__price">price</div>
+        <div className="expense-item__price">$ price</div>
       </div>
     </div>
   );
