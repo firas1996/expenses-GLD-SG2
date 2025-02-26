55757bce6db44e18d1eb1609a53a27165106f264
firas hamila <firas.hm.1996@gmail.com>
2025-02-26 10:29:13 +0100

    a

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,10 +1,14 @@
 import "./ProgressBar.css";
 
-const ProgressBar = ({ label, value }) => {
+const ProgressBar = ({ label, value, max, total }) => {
+  let x = "0%";
+  if (max) {
+    x = (value / max) * 100 + "%";
+  }
   return (
     <div className="stat-bar">
       <div className="stat-bar__inner">
-        <div className="stat-bar__fill"></div>
+        <div className="stat-bar__fill" style={{ height: x }}></div>
       </div>
       <div className="stat-bar__label">{label}</div>
     </div>
